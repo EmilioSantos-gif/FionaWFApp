f593036a2f376870db99651f4f34b990ba461206
Emilio Santos <emilioariel1018@gmail.com>
2022-10-05 08:35:56 -0700

    Primer commit

---
+++ FionaWFApp/frmPrincipal.Designer.cs
@@ -0,0 +1,383 @@
+﻿
+namespace FionaWFApp
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTipoDocumento = new System.Windows.Forms.Label();
+            this.txtTipoDocumento = new System.Windows.Forms.TextBox();
+            this.txtNoDocumento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSector = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.txtValorPerdida = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancellar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTipoDocumento
+            // 
+            this.lblTipoDocumento.AutoSize = true;
+            this.lblTipoDocumento.Location = new System.Drawing.Point(40, 90);
+            this.lblTipoDocumento.Name = "lblTipoDocumento";
+            this.lblTipoDocumento.Size = new System.Drawing.Size(86, 13);
+            this.lblTipoDocumento.TabIndex = 0;
+            this.lblTipoDocumento.Text = "Tipo Documento";
+            // 
+            // txtTipoDocumento
+            // 
+            this.txtTipoDocumento.Location = new System.Drawing.Point(43, 106);
+            this.txtTipoDocumento.Name = "txtTipoDocumento";
+            this.txtTipoDocumento.Size = new System.Drawing.Size(83, 20);
+            this.txtTipoDocumento.TabIndex = 1;
+            // 
+            // txtNoDocumento
+            // 
+            this.txtNoDocumento.Location = new System.Drawing.Point(43, 157);
+            this.txtNoDocumento.Name = "txtNoDocumento";
+            this.txtNoDocumento.Size = new System.Drawing.Size(210, 20);
+            this.txtNoDocumento.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No. Documento";
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(43, 263);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(210, 20);
+            this.txtApellidos.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Apellidos";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(43, 212);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(210, 20);
+            this.txtNombres.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombres";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(43, 435);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(210, 20);
+            this.txtTelefono.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Telefono";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(43, 375);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(210, 20);
+            this.txtDireccion.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Direccion";
+            // 
+            // txtSector
+            // 
+            this.txtSector.Location = new System.Drawing.Point(43, 320);
+            this.txtSector.Name = "txtSector";
+            this.txtSector.Size = new System.Drawing.Size(210, 20);
+            this.txtSector.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Sector";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(313, 375);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(210, 20);
+            this.txtEstado.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(310, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Estado";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(313, 320);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(210, 20);
+            this.txtDescripcion.TabIndex = 23;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(310, 304);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Descripcion";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(310, 247);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(116, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Valor Perdida Estimado";
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(313, 212);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(73, 20);
+            this.txtSexo.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(310, 196);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Sexo";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(310, 141);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(108, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Fecha de Nacimiento";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(313, 106);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(210, 20);
+            this.txtCorreo.TabIndex = 15;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(310, 90);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(38, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Correo";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(313, 157);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaNacimiento.TabIndex = 26;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // txtValorPerdida
+            // 
+            this.txtValorPerdida.Location = new System.Drawing.Point(313, 263);
+            this.txtValorPerdida.Name = "txtValorPerdida";
+            this.txtValorPerdida.Size = new System.Drawing.Size(210, 20);
+            this.txtValorPerdida.TabIndex = 27;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(25, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(664, 36);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "REGISTRO PERSONAS AFECTADAS POR HURACAN FIONA";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(313, 410);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(91, 48);
+            this.btnGuardar.TabIndex = 29;
+            this.btnGuardar.Text = "Guarda";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancellar
+            // 
+            this.btnCancellar.Location = new System.Drawing.Point(432, 407);
+            this.btnCancellar.Name = "btnCancellar";
+            this.btnCancellar.Size = new System.Drawing.Size(91, 48);
+            this.btnCancellar.TabIndex = 30;
+            this.btnCancellar.Text = "Cancelar";
+            this.btnCancellar.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.btnCancellar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtValorPerdida);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSector);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNoDocumento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTipoDocumento);
+            this.Controls.Add(this.lblTipoDocumento);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTipoDocumento;
+        private System.Windows.Forms.TextBox txtTipoDocumento;
+        private System.Windows.Forms.TextBox txtNoDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSector;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValorPerdida;
+        private System.Windows.Forms.Button btnCancellar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ FionaWFApp/frmPrincipal.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+namespace FionaWFApp
+{
+    public partial class frmPrincipal : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Environment.MachineName);
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            String TipoDocumento, NoDocumento, Nombres, Apellidos, Sector, Direccion, Telefono, Correo, Sexo, Descripcion, Estado;
+            DateTime FechaNacimiento;
+            decimal ValorEstimadoPerdida;
+
+            TipoDocumento = txtTipoDocumento.Text;
+            NoDocumento = txtNoDocumento.Text;
+            Nombres = txtNombres.Text;
+            Apellidos = txtApellidos.Text;
+            Sector = txtSector.Text;
+            Direccion = txtDireccion.Text;
+            Telefono = txtTelefono.Text;
+            Correo = txtCorreo.Text;
+            FechaNacimiento = dtpFechaNacimiento.Value;
+            Sexo = txtSexo.Text;
+            if(!decimal.TryParse(txtValorPerdida.Text, out ValorEstimadoPerdida))
+            {
+                MessageBox.Show($"No se pudo parsear el valor perdida: {txtValorPerdida.Text}");
+            }
+            Descripcion = txtDescripcion.Text;
+            Estado = txtEstado.Text;
+
+            Persona persona = new Persona
+            {
+                TipoDocumento = TipoDocumento,
+                NoDocumento = NoDocumento,
+                Nombres = Nombres,
+                Apellidos = Apellidos,
+                Sector = Sector,
+                Direccion = Direccion,
+                Telefono = Telefono,
+                Correo = Correo,
+                FechaNacimiento = FechaNacimiento,
+                Sexo = Sexo,
+                ValorEstimadoPerdida = ValorEstimadoPerdida,
+                Descripcion = Descripcion,
+                Estado = Estado
+            };
+
+            FionaWFAppBDEntities context = new FionaWFAppBDEntities();
+            context.Persona.Add(persona);
+
+            //context.Persona.Add(newPersona());
+
+            try
+            {
+                // Your code...
+                // Could also be before try if you know the exception occurs in SaveChanges
+
+                if(context.SaveChanges() == 1)
+                {
+                    MessageBox.Show("Persona registrada satisfactoriamente");
+
+                    log.Info($"Se registro persona: {persona}");
+                } else
+                {
+                    MessageBox.Show("No se pudo registrar la persona");
+                }
+            }
+            catch (DbEntityValidationException err)
+            {
+                foreach (var eve in err.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                //throw;
+            }
+        }
+
+        private Persona newPersona()
+        {
+            return new Persona
+            {
+                TipoDocumento = "C",
+                NoDocumento = "40219326557",
+                Nombres = "Alejandro",
+                Apellidos = "Rodriguez",
+                Sector = "Maria Auxiliadora",
+                Direccion = "Calle primera, casa #2",
+                Telefono = "8095864715",
+                Correo = "alexrodriguez@gmail.com",
+                FechaNacimiento = new DateTime(),
+                Sexo = "M",
+                ValorEstimadoPerdida = 152000.12m,
+                Descripcion = "Perdidas provocadas a vivienda",
+                Estado = "Pendiente"
+            };
+        }
+    }
+}
